b09a0c6f7ee18efd2d5b611c485e610a8a8e6923
an <d@mail.ru>
2020-06-11 19:42:47 +0900

    1

--- hello.py
+++ hello.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+import datetime
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', page_header='Hello world')
+    return render_template('index.html', page_header='Hello world', current_time=datetime.datetime.utcnow())
 
 
 @app.route('/user/<username>')
